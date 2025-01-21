9155c5b633339fc949807122f5031f2a1dd48f51
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-01-21 19:37:18 -0300

    implement random cat fact feature with image display

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { API_FACT_URL, API_CAT_URL} from './constants'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [change, setChange] = useState(false)
+  const [fact, setFact] = useState('')
+  const [imgUrl, setImgUrl] = useState('')
+  
+  const handleClick = () => {
+    setChange(prevChange => !prevChange)
+  }
+
+  useEffect(() => {
+    setImgUrl('')
+    async function getRamdonFact(){
+        const response = await fetch(API_FACT_URL)
+        const json = await response.json()
+        const data = json.fact
+        setFact(data)
+    } 
+
+    getRamdonFact()
+
+  }, [change])
+
+  useEffect(() => {
+    const phrase = fact.split(' ')[0]
+
+    const newApiUrl = `${API_CAT_URL}${phrase}`
+    console.log('newApiUrl:', newApiUrl)
+    setImgUrl(newApiUrl)
+  },[fact])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <button onClick={handleClick}>Change cat</button>
+      <div className="img_container">
+      {
+          imgUrl !== ''
+          ? <img src={imgUrl} alt='cat' />
+          : <p>Loading...</p>
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
+        {
+          fact
+          ? <p>{fact}</p>
+          : <p>Loading...</p>
+        }
+      
+    </main>
+     
+     
     </>
   )
 }
